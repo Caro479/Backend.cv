da355a387d8a39c063a0b0fcc30260b204842487
Caro479 <111710224+Caro479@users.noreply.github.com>
2023-01-17 08:35:13 -0300

    cors

--- src/main/java/com/porfolio/vc/Security/Controller/AuthController.java
+++ src/main/java/com/porfolio/vc/Security/Controller/AuthController.java
@@ -32,8 +32,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://frontendcv123.web.app","http://localhost:4200"})
-/*@CrossOrigin(origins = "http://localhost:4200")*/
+/*@CrossOrigin(origins = {"https://frontendcv123.web.app","http://localhost:4200"})*/
+@CrossOrigin(origins = "https://frontendcv123.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
